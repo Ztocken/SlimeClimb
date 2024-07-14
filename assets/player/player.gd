64662af1fcc8bdd314a553eaf151c5b8a78065f6
Simon Malinen <Simon.malinen@gmail.com>
2024-07-14 13:52:08 +0200

    Fixed wall slide bug and implemented A & D Support on Keyboard

--- assets/player/player.gd
+++ assets/player/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 const SPEED = 150.0
-const JUMP_VELOCITY = -250.0
+const JUMP_VELOCITY = -300.0
 const WALL_JUMP_HORIZONTAL_VELOCITY = 200.0  # Horizontal velocity for wall jumps
 const WALL_SLIDE_GRAVITY = 50.0  # Reduced gravity for wall sliding
 
@@ -17,44 +17,46 @@ var is_wall_jumping = false
 @export var stuckwall_texture: Texture2D
 var maxDoubleJumpCount = 1
 var doublejumpCount = 0;
+
 func _physics_process(delta):
 	# Check if the player is touching a wall.
 	var is_touching_wall = raycastRight.is_colliding() or raycastLeft.is_colliding()
-	print(doublejumpCount,"/",maxDoubleJumpCount)
 	sprite.flip_v = false
+	#print(velocity.y)
 	# Add the gravity.
 	if is_touching_wall and not is_on_floor():
 		doublejumpCount = 0
 		sprite.texture = stuckwall_texture
 		sprite.flip_h = raycastLeft.is_colliding()
 		# Reduce gravity when touching a wall to simulate sliding.
-		velocity.y += WALL_SLIDE_GRAVITY * delta
-		if Input.is_action_just_pressed("ui_left") and not raycastRight.is_colliding():
+		if velocity.y < 1:
+			velocity.y += (gravity  * delta)
+		else:
+			velocity.y += (gravity / 12  * delta)
+		if Input.is_action_just_pressed("Left") and not raycastRight.is_colliding():
 			velocity = Vector2(-WALL_JUMP_HORIZONTAL_VELOCITY, JUMP_VELOCITY)
-		elif Input.is_action_just_pressed("ui_right") and not raycastLeft.is_colliding():
+		elif Input.is_action_just_pressed("Right") and not raycastLeft.is_colliding():
 			velocity = Vector2(WALL_JUMP_HORIZONTAL_VELOCITY, JUMP_VELOCITY)
 	else:
-		velocity.y += gravity * delta
 		sprite.texture = falling_texture
+		velocity.y += (gravity * delta)
 		if doublejumpCount < maxDoubleJumpCount:
-			if Input.is_action_just_pressed("ui_left") and not raycastRight.is_colliding():
+			if Input.is_action_just_pressed("Left") and not raycastRight.is_colliding():
 				velocity = Vector2(-WALL_JUMP_HORIZONTAL_VELOCITY, JUMP_VELOCITY)
 				doublejumpCount += 1
-			elif Input.is_action_just_pressed("ui_right") and not raycastLeft.is_colliding():
+			elif Input.is_action_just_pressed("Right") and not raycastLeft.is_colliding():
 				velocity = Vector2(WALL_JUMP_HORIZONTAL_VELOCITY, JUMP_VELOCITY)
 				doublejumpCount += 1
-		
-			
 
 	if is_on_floor():
 		velocity.x = 0
 		is_wall_jumping = false
 		sprite.texture = normal_texture
 		# Handle horizontal movement.
-		if Input.is_action_just_pressed("ui_left") and not raycastRight.is_colliding():
+		if Input.is_action_just_pressed("Left") and not raycastRight.is_colliding():
 			velocity.x = -SPEED
 			velocity.y = JUMP_VELOCITY
-		elif Input.is_action_just_pressed("ui_right") and not raycastLeft.is_colliding():
+		elif Input.is_action_just_pressed("Right") and not raycastLeft.is_colliding():
 			velocity.x = SPEED
 			velocity.y = JUMP_VELOCITY
 	move_and_slide()
